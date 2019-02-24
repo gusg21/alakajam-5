4b71991576b6bef85c60dda1dafec49dd1039d4b
gusg21 <gusg21@parkschool.net>
2019-02-23 21:48:38 -0500

    rendering players!!!

--- Alakajam/PlayState.cs
+++ Alakajam/PlayState.cs
@@ -15,18 +15,31 @@ namespace Alakajam
 	{
 		Texture2D background;
 		BreaddedCamera camera;
+		GameObjectGroup objects;
+
+		Player player1; Player player2;
 
 		public PlayState(ContentManager content)
 		{
 			background = content.Load<Texture2D> ("Images/background");
+
 			camera = new BreaddedCamera (640, 480);
 			camera.Zoom = 2F;
 			camera.MoveTo (new Vector2 (160, 120));
+
+			objects = new GameObjectGroup ();
+
+			player1 = new Player (PlayerNumber.ONE, content);
+			objects.Add (player1);
+			player2 = new Player (PlayerNumber.TWO, content);
+			objects.Add (player2);
 		}
 
 		public override void Update(GameTime gameTime)
 		{
 			Parent.TransformMatrix = camera.Transform;
+
+			objects.Update (gameTime);
 
 			base.Update (gameTime);
 		}
@@ -34,6 +47,8 @@ namespace Alakajam
 		public override void Draw(SpriteBatch batch)
 		{
 			batch.Draw (background, Vector2.Zero, Color.White);
+
+			objects.Draw (batch);
 
 			base.Draw (batch);
 		}
---
+++ Alakajam/Player.cs
@@ -0,0 +1,46 @@
+﻿using BS;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alakajam
+{
+	class Player : MotionGameObject
+	{
+		PlayerNumber number;
+		Texture2D tex;
+
+		public Player(PlayerNumber number, ContentManager content) : base()
+		{
+			this.number = number;
+
+			if (number == PlayerNumber.ONE)
+			{
+				position = new Vector2 (1, 31);
+			}
+			else if (number == PlayerNumber.TWO)
+			{
+				position = new Vector2 (291, 31);
+			}
+
+			tex = content.Load<Texture2D> ("Images/player-" + (number == PlayerNumber.ONE ? "blue" : "green"));
+		}
+
+		public override void Draw(SpriteBatch batch)
+		{
+			batch.Draw (tex, position, Color.White);
+
+			base.Draw (batch);
+		}
+	}
+
+	enum PlayerNumber
+	{
+		ONE, TWO
+	}
+}
